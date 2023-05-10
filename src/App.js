8817b60c2cdc07249c0aaa354cddf1851ec22175
sonnenbergmarc <msonnenberg@live.com>
2023-05-09 19:02:35 -0600

    Created a db.json and changed createBook

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
+import axios from "axios";
 
 function App() {
     const [books, setBooks] = useState([]);
@@ -25,8 +26,11 @@ function App() {
         setBooks(updatedBooks);
     };
 
-    const createBook = (title) => {
-        const updatedBooks = [...books, {id: Math.round(Math.random() * 9999), title,}];
+    const createBook = async (title) => {
+        const response = await axios.post('http://localhost:3001/books', {
+            title
+        });
+        const updatedBooks = [...books, response.data];
         setBooks(updatedBooks);
     };
 
